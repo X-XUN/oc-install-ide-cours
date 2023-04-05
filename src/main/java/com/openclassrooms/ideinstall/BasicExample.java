25e9bfa926500e161a01498c94dcefdee87a0f73
x <xudanfeng@localhost>
2023-04-05 17:56:16 +0800

    test

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,8 +3,8 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		int i;
-		for (i = 0; i < 3; i++) {
+		int in;
+		for (in = 0; in < 3; in++) {
 			System.out.println("Hello bugs !");
 		}
 	}
